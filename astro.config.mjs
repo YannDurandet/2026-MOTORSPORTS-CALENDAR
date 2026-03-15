a74b5314fccce464bdf209d13a5132f0da69d180
Yann Durandet <durandet.yann@gmail.com>
2026-03-15 23:20:20 +0100

    switch to astro

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://yanndurandet.github.io',
+  base: '/2026-MOTORSPORTS-CALENDAR',
+  build: {
+    format: 'file',
+  },
+});
